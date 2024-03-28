1dda8e221e805d1ca18703713a3c738400785c41
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-03-28 03:20:37 -0800

    6th commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
+ <script src="https://cdn.tailwindcss.com"></script>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css" integrity="sha512-MqL4+Io386IOPMKKyplKII0pVW5e+kb+PI/I3N87G3fHIfrgNNsRpzIXEi+0MQC0sR9xZNqZqCYVcC61fL5+Vg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className={mons.className}>{children}</body>
