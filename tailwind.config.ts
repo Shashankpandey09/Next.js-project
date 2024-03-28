a94740b46db4f2aeaeeeaa21792772005a6564a1
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-03-28 03:47:29 -0800

    7th commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: 'jit', // Add JIT mode here
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
